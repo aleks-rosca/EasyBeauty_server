68b1e804952b4b144666d9f7c5c7b90cb0ca7aeb
Alexandru Rosca <aro@cedas.dk>
2021-10-14 17:37:03 +0200

    delete

---
+++ EasyBeauty_server/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBeauty_server.Models
+{
+    public class Response
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+    }
+}
