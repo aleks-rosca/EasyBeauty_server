2cfb2a3156437af0f50a2225410ff73e6e8cd8ea
Alexandru Rosca <aro@cedas.dk>
2021-11-05 15:40:43 +0100

    payment

--- EasyBeauty_server/Controllers/PaymentController.cs
+++ EasyBeauty_server/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyBeauty_server.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,10 @@ namespace EasyBeauty_server.Controllers
     {
 
         [HttpPost]
-        public string Post([FromBody] int amount)
+        public object Post([FromBody] Payment amount)
         {
             var random = new Random();
-            var list = new List<string> { "Payment Successfull", "Incorrect Pin Code", "Insuficient Funds" };
+            var list = new List<object> { new { succes = "Payment Successfull" }, new { error = "Incorrect Pin Code" }, new { error = "Insuficient Funds" }, new { error = "Unknown error"}, new { error = "Something went wrong" } };
             int i = random.Next(list.Count);
             return list[i];
         }
---
+++ EasyBeauty_server/Models/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBeauty_server.Models
+{
+    public class Payment
+    {
+        public double Value { get; set; }
+    }
+}
