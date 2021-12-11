c4d6cc84412116c07e7c0eec0ae0cb7a9d695856
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2021-12-11 13:55:45 +0100

    small update

--- EasyBeauty_server/Models/Employee.cs
+++ EasyBeauty_server/Models/Employee.cs
@@ -3,13 +3,6 @@
     public class Employee : Customer
     {
         public int ID { get; set; }
-
-        public new string Email { get; set; }
-
-        public new string Name { get; set; }
-
-        public new int PhoneNr { get; set; }
-
         public string Role { get; set; }
     }
 }
